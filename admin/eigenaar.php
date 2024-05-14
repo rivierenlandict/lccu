4075dafbc7c2bfcf2f8c033f3b32021ce0c999c1
ruben-michiels-police <143076055+ruben-michiels-police@users.noreply.github.com>
2024-05-14 15:16:39 +0200

    expanded the admin page

---
+++ admin/eigenaar.php
@@ -0,0 +1,71 @@
+<?php
+include("../connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eigenaar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        .container{
+            width: 80%;
+            margin: 0px auto;
+            text-align: center;
+            padding: 1em;
+        }
+        h1, h2{
+            padding: 0.5em;
+        }
+  
+   
+
+    </style>
+</head>
+<body>
+<div class="container">
+<a href="index.php"> Terug </a>
+<?php
+$eigenaarid = $_GET["id"];
+echo "<h1> Eigenaar </h1>";
+echo "<table class='table'>";
+echo "<tr><th> Naam </th><th> Voornaam </th><th> Geboortedatum </th><th> Adres </th><th> Gemeente </th><th> Hoedanigheid </th></tr>";
+$sql = "SELECT * FROM eigenaar WHERE Id = '$eigenaarid'";
+$result = $conn->query($sql);
+if($result){
+    echo "<script> console.log('ok'); </script>";
+}
+else{
+    echo "<script> console.log('something went wrong'); </script>";
+}
+while($row = $result->fetch_assoc()){
+    echo "<td> ".$row["Naam"]."</td><td>".$row["Voornaam"]."</td><td>".$row["Geboortedatum"]."</td><td>".$row["Adres"]."</td><td>".$row["Gemeente"]."</td>";
+    $hoedanigheidid = $row["HoedanigheidId"];
+    $materiaalid = $row["MateriaalId"];
+    //Getting the hoedanigheid
+    $sql = "SELECT * FROM hoedanigheid WHERE Id = '$hoedanigheidid'";
+    $result = $conn->query($sql);
+    while($row2 = $result->fetch_assoc()){
+        $hoedanigheid = $row2["Hoedanigheid"];
+    }
+    /////////////
+    echo "<td>".$hoedanigheid."</td></tr>";
+}
+echo "</table>";
+?>
+<h2> Materiaal </h2>
+<?php
+echo "<table class='table'>";
+echo "<tr><th> Model </th><th> Serienummer </th><th> Toegangscode </th><th> SINN </th><th> Extra info </th></tr>";
+$sql = "SELECT * FROM materiaal WHERE Id = '$materiaalid'";
+$result = $conn->query($sql);
+while($row = $result->fetch_assoc()){
+    echo "<tr><td>".$row["Model"]."</td><td>".$row["Serienummer"]."</td><td>".$row["Toegangscode"]."</td><td>".$row["SINN"]."</td><td>".$row["ExtraInfo"]."</td></tr>";
+
+}
+echo "</table>";
+?>
+</div>
+</body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -29,7 +29,7 @@ include("../connect.php");
 <div class="container">
 <h1> Aanvragen </h1>
 <?php
-$sql = "SELECT aanvraag.Team, aanvraag.Dossierbeheerder, aanvraag.Pv, aanvraag.Misdrijf, aanvraag.NaamOr, aanvraag.Inbeslagname, aanvraag.Dossiernaam, aanvraag.Zoekelementen, eigenaar.Naam, eigenaar.Voornaam, afhandeling.Afhandeling FROM aanvraag INNER JOIN eigenaar ON aanvraag.EigenaarId = eigenaar.Id INNER JOIN afhandeling ON aanvraag.AfhandelingId = afhandeling.Id";
+$sql = "SELECT aanvraag.Team, aanvraag.Id, aanvraag.EigenaarId, aanvraag.Dossierbeheerder, aanvraag.Pv, aanvraag.Misdrijf, aanvraag.NaamOr, aanvraag.Inbeslagname, aanvraag.Dossiernaam, aanvraag.Zoekelementen, eigenaar.Naam, eigenaar.Voornaam, afhandeling.Afhandeling FROM aanvraag INNER JOIN eigenaar ON aanvraag.EigenaarId = eigenaar.Id INNER JOIN afhandeling ON aanvraag.AfhandelingId = afhandeling.Id";
 $result = $conn->query($sql);
 if($result){
     echo "<script> console.log('Table is loaded'); </script>";
@@ -37,10 +37,13 @@ if($result){
 else{
     echo "<script> console.log('Something went wrong'); </script>";
 }
+echo "<form method='post'>";
 echo "<table class='table'>";
-echo "<tr><th> Team </th><th> Dossierbeheerder </th><th> PV </th><th> Misdrijf </th><th> OR/PDK </th><th> Datum inbeslagname </th><th> Dossiernaam </th><th> Eigenaar </th><th> Afhandeling </th><th> Zoekelementen </th></tr>";
+echo "<tr><th> Team </th><th> Dossierbeheerder </th><th> PV </th><th> Misdrijf </th><th> OR/PDK </th><th> Datum inbeslagname </th><th> Dossiernaam </th><th> Eigenaar </th><th> Afhandeling </th><th> Zoekelementen </th><th> Acties </th></tr>";
+$tellerDelete = 0;
 while($row = $result->fetch_assoc()){
-    echo "<tr><td>".$row["Team"]."</td><td>".$row["Dossierbeheerder"]."</td><td>".$row["Pv"]."</td><td>".$row["Misdrijf"]."</td><td>".$row["NaamOr"]."</td><td>".$row["Inbeslagname"]."</td><td>".$row["Dossiernaam"]."</td><td><a href='#'>".$row["Naam"]."</a></td>
+    $idrij= $row["Id"];
+    echo "<tr><td>".$row["Team"]."</td><td>".$row["Dossierbeheerder"]."</td><td>".$row["Pv"]."</td><td>".$row["Misdrijf"]."</td><td>".$row["NaamOr"]."</td><td>".$row["Inbeslagname"]."</td><td>".$row["Dossiernaam"]."</td><td><a href='eigenaar.php?id=".$row["EigenaarId"]."'>".$row["Naam"]."</a></td>
     <td>";
     if($row["Afhandeling"] == "Gewone afhandeling"){
         echo "<span style='color:green;'>".$row["Afhandeling"]."</span>";
@@ -51,10 +54,35 @@ while($row = $result->fetch_assoc()){
     else if($row["Afhandeling"] == "Prioritair"){
         echo "<span style='color:red;'>".$row["Afhandeling"]."</span>";
     }
-    echo "</td><td>".$row["Zoekelementen"]."</td></tr>";
-
+    echo "</td><td>".$row["Zoekelementen"]."</td><td> <button name='btnDelete".$tellerDelete."' value='".$row["Id"]."' class='btn btn-danger'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+    <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>
+    <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>
+  </svg></button></td></tr>";
+  $tellerDelete++;
 }
 echo "</table>";
+echo "</form>";
+
+for($i=0;$i<$tellerDelete;$i++){
+    if(isset($_POST["btnDelete".$i])){
+        $teverwijderenid = $_POST["btnDelete".$i];
+        $sql = "DELETE FROM aanvraag WHERE Id = '$teverwijderenid'";
+        $result = $conn->query($sql);
+        if($result){
+            echo "<script> window.location.reload(); </script>";
+            echo "Record verwijderd uit aanvraag";
+        }
+        else{
+            echo "Er is iets misgelopen bij de tabel aanvraag";
+        }
+
+    }
+}
+
+
+
+
+
 ?>
 
 
